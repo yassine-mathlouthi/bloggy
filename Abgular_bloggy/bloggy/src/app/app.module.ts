d8dbc4a279b0f5fd97a7fe432ab1e711790cc25d
screen-t <yassinemt08@gmail.com>
2023-03-24 04:47:37 +0100

    nav bar,404page and creation for home page

--- Abgular_bloggy/bloggy/src/app/app.module.ts
+++ Abgular_bloggy/bloggy/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PageNotFoundComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
