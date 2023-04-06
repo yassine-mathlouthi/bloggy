40dde3dc3474c8c0df9c8f4a1ac2d75b6ae6adb0
mouhamedazizkbaier05 <azizm9592@gmail.com>
2023-04-07 00:24:55 +0200

    rani zedet add blogg

---
+++ Abgular_bloggy/bloggy/src/app/insertblogg/insertblogg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insertblogg',
+  templateUrl: './insertblogg.component.html',
+  styleUrls: ['./insertblogg.component.css']
+})
+export class InsertbloggComponent {
+
+}
